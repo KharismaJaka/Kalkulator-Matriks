c9670abe7763fa16a10439626d96a92040c5e9e6
Kharisma Jaka Harum <kjakaharum@gmail.com>
2024-12-10 20:02:02 +0700

    Menambahkan fungsi pengurangan
    fungsi ini berfungsi agar kedua matriks dapat melakukan operasi pengurangan

--- program.cpp
+++ program.cpp
@@ -26,4 +26,12 @@ void tambah(int matriks1[10][10], int matriks2[10][10], int hasil[10][10], int b
     }
 }
 
+void kurang(int matriks1[10][10], int matriks2[10][10], int hasil[10][10], int baris, int kolom) {
+    for (int i = 0; i < baris; i++) {
+        for (int j = 0; j < kolom; j++) {
+            hasil[i][j] = matriks1[i][j] - matriks2[i][j];
+        }
+    }
+}
+
 
