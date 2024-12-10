fbeff94a37e50c13b1403c7f264f720d1ded0da7
Kharisma Jaka Harum <kjakaharum@gmail.com>
2024-12-10 19:37:35 +0700

    menyiapkan fungsi input
    fungsi ini berfungsi untuk user dapat memasukkan angka yang diinginkan pada matriks

--- program.cpp
+++ program.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+void masuk(int matriks[10][10], int baris, int kolom) {
+    for (int i = 0; i < kolom; i++) {
+        for (int j = 0; j < kolom; j++) {
+            cin >> matriks[i][j];
+        }
+    }
+}
