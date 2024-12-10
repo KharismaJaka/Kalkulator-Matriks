47f39914bfdd057c5cbf11f18f14397e2d0f91bd
Kharisma Jaka Harum <kjakaharum@gmail.com>
2024-12-10 19:42:48 +0700

    menyiapkan fungsi tambah
    fungsi ini berfungsi untuk menjumlahkan kedua matriks

--- program.cpp
+++ program.cpp
@@ -17,3 +17,13 @@ void keluar(int matriks[10][10], int baris, int kolom) {
         cout << endl;
     }
 }
+
+void tambah(int matriks1[10][10], int matriks2[10][10], int hasil[10][10], int baris, int kolom) {
+    for (int i = 0; i < baris; i++) {
+        for (int j = 0; j < kolom; j++) {
+            hasil[i][j] = matriks1[i][j] + matriks2[i][j];
+        }
+    }
+}
+
+
